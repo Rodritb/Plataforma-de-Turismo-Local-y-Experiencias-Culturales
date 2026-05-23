4aa154cf690103d7e237064e2e81321d21af5b14
alexander <130918131+alekeysmith@users.noreply.github.com>
2026-05-22 21:43:02 -0400

    Use environment variable for server port
    Update app to use environment variable for port

--- server/app.py
+++ server/app.py
@@ -458,4 +458,6 @@ def delete_menu_item(id):
     return jsonify({"message": "Ítem eliminado"})
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    # Render asigna el puerto mediante una variable de entorno llamada PORT
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
