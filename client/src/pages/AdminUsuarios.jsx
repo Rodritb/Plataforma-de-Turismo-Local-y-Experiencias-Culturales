d5711ff06289fb57441338b4c4598d588a31decf
Rodritb <142846827+Rodritb@users.noreply.github.com>
2026-06-12 17:45:50 -0400

    Update AdminUsuarios.jsx

--- client/src/pages/AdminUsuarios.jsx
+++ client/src/pages/AdminUsuarios.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_URL = "http://localhost:3000/api/users";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/auth/login`;
 
 function getToken() {
   return localStorage.getItem("token");
